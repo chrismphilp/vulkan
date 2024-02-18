abf852d14cb2f2b4ca459dcca38bcdf11593234a
Chris Philp <chris.philp@starlingbank.com>
2024-02-18 18:36:20 +0000

    [master] | Adding initial code for rendering window

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-#![allow(
-dead_code,
-unused_variables,
-clippy::too_many_arguments,
-clippy::unnecessary_wraps
-)]
+use std::sync::Arc;
 
-use anyhow::{anyhow, Result};
-use vulkanalia::{Entry, Instance, vk};
-use vulkanalia::loader::{LibloadingLoader, LIBRARY};
-use vulkanalia::vk::HasBuilder;
-use vulkanalia::window as vk_window;
-use winit::dpi::LogicalSize;
+use anyhow::Result;
+use vulkano::{swapchain, sync, VulkanLibrary};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, RenderPassBeginInfo, SubpassContents};
+use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
+use vulkano::device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo};
+use vulkano::device::physical::PhysicalDeviceType;
+use vulkano::image::{ImageAccess, SwapchainImage};
+use vulkano::image::view::ImageView;
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::pipeline::graphics::viewport::Viewport;
+use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass};
+use vulkano::swapchain::{AcquireError, Swapchain, SwapchainCreateInfo, SwapchainCreationError, SwapchainPresentInfo};
+use vulkano::sync::{FlushError, GpuFuture};
+use vulkano_win::VkSurfaceBuild;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
@@ -18,83 +21,264 @@ use winit::window::{Window, WindowBuilder};
 const WIDTH: u16 = 500;
 const HEIGHT: u16 = 500;
 
+// https://taidaesal.github.io/vulkano_tutorial/section_1.html
 fn main() -> Result<()> {
     pretty_env_logger::init();
 
-    // Window
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("Vulkan Tutorial (Rust)")
-        .with_inner_size(LogicalSize::new(WIDTH, HEIGHT))
-        .build(&event_loop)?;
+    let mut recreate_swapchain = false;
+    let library = VulkanLibrary::new()
+        .expect("no local Vulkan library/DLL");
+    let extensions = vulkano_win::required_extensions(&library);
 
-    // App
-    let mut app: App = unsafe { App::create(&window)? };
-    let mut destroying = false;
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
-        match event {
-            // Render a frame if our Vulkan app is not being destroyed.
-            Event::MainEventsCleared if !destroying =>
-                unsafe { app.render(&window) }.unwrap(),
-            // Destroy our Vulkan app.
-            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
-                destroying = true;
-                *control_flow = ControlFlow::Exit;
-                unsafe { app.destroy(); }
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            enabled_extensions: extensions,
+            enumerate_portability: true,
+            ..InstanceCreateInfo::default()
+        },
+    ).expect("Failed to create instance");
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .unwrap();
+
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::empty()
+    };
+
+    let (physical_device, queue_family_index) = instance
+        .enumerate_physical_devices()
+        .unwrap()
+        .filter(|p| p.supported_extensions().contains(&device_extensions))
+        .filter_map(|p| {
+            p.queue_family_properties()
+                .iter()
+                .enumerate()
+                .position(|(i, q)| {
+                    // pick first queue_family_index that handles graphics and can draw on the surface created by winit
+                    q.queue_flags.graphics && p.surface_support(i as u32, &surface).unwrap_or(false)
+                })
+                .map(|i| (p, i as u32))
+        })
+        .min_by_key(|(p, _)| {
+            // lower score for preferred device types
+            match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5,
             }
-            _ => {}
+        })
+        .expect("No suitable physical device found");
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo {
+            enabled_extensions: device_extensions,
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        })
+        .expect("Could not create device");
+
+    let queue = queues.next()
+        .expect("Unable to find the next queue item");
+
+    let (mut swapchain, images) = {
+        let caps = device
+            .physical_device()
+            .surface_capabilities(&surface, Default::default())
+            .unwrap();
+
+        let usage = caps.supported_usage_flags;
+        let alpha = caps.supported_composite_alpha.iter().next().unwrap();
+
+        let image_format = Some(
+            device
+                .physical_device()
+                .surface_formats(&surface, Default::default())
+                .expect("Could not surface formats for device")[0]
+                .0,
+        );
+
+        let window = surface.object().unwrap().downcast_ref::<Window>().unwrap();
+        let image_extent: [u32; 2] = window.inner_size().into();
+
+        Swapchain::new(
+            device.clone(),
+            surface.clone(),
+            SwapchainCreateInfo {
+                min_image_count: caps.min_image_count,
+                image_format,
+                image_extent,
+                image_usage: usage,
+                composite_alpha: alpha,
+                ..Default::default()
+            },
+        ).expect("Failed to create Swapchain")
+    };
+
+    let command_buffer_allocator =
+        StandardCommandBufferAllocator::new(device.clone(), Default::default());
+
+    let render_pass = vulkano::single_pass_renderpass!(
+    device.clone(),
+    attachments: {
+        color: {
+            load: Clear,
+            store: Store,
+            format: swapchain.image_format(),
+            samples: 1,
         }
+    },
+    pass: {
+        color: [color],
+        depth_stencil: {}
+    }
+    ).expect("Unable to create a render pass");
+
+    let mut viewport = Viewport {
+        origin: [0.0, 0.0],
+        dimensions: [0.0, 0.0],
+        depth_range: 0.0..1.0,
+    };
+
+    let mut framebuffers = window_size_dependent_setup(&images, render_pass.clone(), &mut viewport);
+    let mut previous_frame_end = Some(Box::new(sync::now(device.clone())) as Box<dyn GpuFuture>);
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(_),
+            ..
+        } => {
+            recreate_swapchain = true;
+        }
+        Event::RedrawEventsCleared => {
+            previous_frame_end
+                .as_mut()
+                .take()
+                .unwrap()
+                .cleanup_finished();
+
+            if recreate_swapchain {
+                let window = surface.object().unwrap().downcast_ref::<Window>().unwrap();
+                let image_extent: [u32; 2] = window.inner_size().into();
+
+                let (new_swapchain, new_images) = match swapchain.recreate(SwapchainCreateInfo {
+                    image_extent,
+                    ..swapchain.create_info()
+                }) {
+                    Ok(r) => r,
+                    Err(SwapchainCreationError::ImageExtentNotSupported { .. }) => return,
+                    Err(e) => panic!("Failed to recreate swapchain: {:?}", e),
+                };
+
+                swapchain = new_swapchain;
+                framebuffers = window_size_dependent_setup(&new_images, render_pass.clone(), &mut viewport);
+                recreate_swapchain = false;
+            }
+
+            let (image_index, suboptimal, acquire_future) =
+                match swapchain::acquire_next_image(swapchain.clone(), None) {
+                    Ok(r) => r,
+                    Err(AcquireError::OutOfDate) => {
+                        recreate_swapchain = true;
+                        return;
+                    }
+                    Err(e) => panic!("Failed to acquire next image: {:?}", e),
+                };
+
+            if suboptimal {
+                recreate_swapchain = true;
+            }
+
+            let clear_values = vec![Some([0.5, 0.5, 1.0, 0.5].into())];
+
+            let mut cmd_buffer_builder = AutoCommandBufferBuilder::primary(
+                &command_buffer_allocator,
+                queue.queue_family_index(),
+                CommandBufferUsage::OneTimeSubmit,
+            )
+                .unwrap();
+
+            cmd_buffer_builder
+                .begin_render_pass(
+                    RenderPassBeginInfo {
+                        clear_values,
+                        ..RenderPassBeginInfo::framebuffer(
+                            framebuffers[image_index as usize].clone(),
+                        )
+                    },
+                    SubpassContents::Inline,
+                )
+                .unwrap()
+                .end_render_pass()
+                .unwrap();
+
+            let command_buffer = cmd_buffer_builder.build().unwrap();
+
+            let future = previous_frame_end
+                .take()
+                .unwrap()
+                .join(acquire_future)
+                .then_execute(queue.clone(), command_buffer)
+                .unwrap()
+                .then_swapchain_present(
+                    queue.clone(),
+                    SwapchainPresentInfo::swapchain_image_index(swapchain.clone(), image_index),
+                )
+                .then_signal_fence_and_flush();
+
+            match future {
+                Ok(future) => {
+                    previous_frame_end = Some(Box::new(future) as Box<_>);
+                }
+                Err(FlushError::OutOfDate) => {
+                    recreate_swapchain = true;
+                    previous_frame_end = Some(Box::new(sync::now(device.clone())) as Box<_>);
+                }
+                Err(e) => {
+                    println!("Failed to flush future: {:?}", e);
+                    previous_frame_end = Some(Box::new(sync::now(device.clone())) as Box<_>);
+                }
+            }
+        }
+        _ => {}
     });
 }
 
-unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
-    let application_info = vk::ApplicationInfo::builder()
-        .application_name(b"Vulkan Tutorial\0")
-        .application_version(vk::make_version(1, 0, 0))
-        .engine_name(b"No Engine\0")
-        .engine_version(vk::make_version(1, 0, 0))
-        .api_version(vk::make_version(1, 0, 0));
+fn window_size_dependent_setup(
+    images: &[Arc<SwapchainImage>],
+    render_pass: Arc<RenderPass>,
+    viewport: &mut Viewport,
+) -> Vec<Arc<Framebuffer>> {
+    let dimensions = images[0].dimensions().width_height();
+    viewport.dimensions = [dimensions[0] as f32, dimensions[1] as f32];
 
-    let extensions = vk_window::get_required_instance_extensions(window)
+    images
         .iter()
-        .map(|e| e.as_ptr())
-        .collect::<Vec<_>>();
-
-    let info = vk::InstanceCreateInfo::builder()
-        .application_info(&application_info)
-        .enabled_extension_names(&extensions);
-
-    Ok(entry.create_instance(&info, None)?)
+        .map(|image| {
+            let view = ImageView::new_default(image.clone()).unwrap();
+            Framebuffer::new(
+                render_pass.clone(),
+                FramebufferCreateInfo {
+                    attachments: vec![view],
+                    ..Default::default()
+                },
+            ).expect("Unable to collect images for Swapchain")
+        })
+        .collect::<Vec<_>>()
 }
-
-/// Our Vulkan app.
-#[derive(Clone, Debug)]
-struct App {
-    entry: Entry,
-    instance: Instance,
-}
-
-impl App {
-    /// Creates our Vulkan app.
-    unsafe fn create(window: &Window) -> Result<Self> {
-        let loader = LibloadingLoader::new(LIBRARY)?;
-        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
-        let instance = create_instance(window, &entry)?;
-        Ok(Self { entry, instance })
-    }
-
-    /// Renders a frame for our Vulkan app.
-    unsafe fn render(&mut self, window: &Window) -> Result<()> {
-        Ok(())
-    }
-
-    /// Destroys our Vulkan app.
-    unsafe fn destroy(&mut self) {
-        self.instance.destroy_instance(None);
-    }
-}
-
-/// The Vulkan handles and associated properties used by our Vulkan app.
-#[derive(Clone, Debug, Default)]
-struct AppData {}
